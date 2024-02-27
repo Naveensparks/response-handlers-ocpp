d475c4a46c6f578687936104273fdc00ffe70812
Naveen Kumar M <47047373+Naveensparks@users.noreply.github.com>
2024-02-27 09:08:40 +0530

    Update index.js

--- StopTransaction/index.js
+++ StopTransaction/index.js
@@ -1,7 +1,10 @@
 module.exports = (msg, { success, error }, cp) => {
   success({
-      transactionId : 2,
-      timestamp : (new Date()).toISOString(),
-      meterStop : 1
+      idTagInfo: {status: "Accepted"}
+                      /*Accepted",
+                        "Blocked",
+                        "Expired",
+                        "Invalid",
+                        "ConcurrentTx"*/
   });
 };
