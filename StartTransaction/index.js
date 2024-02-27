bc82aa3f537547b1fcfd5d46c8325a4511c1ca8f
Naveen Kumar M <47047373+Naveensparks@users.noreply.github.com>
2024-02-27 09:07:00 +0530

    Update index.js

--- StartTransaction/index.js
+++ StartTransaction/index.js
@@ -1,8 +1,6 @@
 module.exports = (msg, { success, error }, cp) => {
   success({
-       connectorId : 2,
-       idTag : 'string20',
-       meterStart : 1,
-       timestamp : (new Date()).toISOString()
+    transactionId: 12134,
+    idTagInfo: {status: "Accepted"}  
 });
 };
